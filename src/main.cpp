41971f7e512974ed40cc6b518e0952c21aa17034
Jakub Nagy <nagykubko@gmail.com>
2023-04-01 13:46:13 +0200

    Enable LED

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
+#include <Wire.h>
 
+#define LED1202_GLOBAL_7BIT_ADDR  (0x5C)
+#define LED1202_DEVICE_ENABLE      ((uint8_t)0x01)
+
+void writeReg(uint8_t add, uint8_t reg, uint8_t data)
+{
+    Wire.beginTransmission(add);
+    Wire.write(reg);                          
+    Wire.write(data);                        
+    Wire.endTransmission();
+} 
+ 
 void setup()
 {
+    Wire.begin();
     Serial.begin(115200);
+    writeReg(LED1202_GLOBAL_7BIT_ADDR, LED1202_DEVICE_ENABLE, 0x01);
 }
-
+ 
 void loop()
 {
-    Serial.println("Hello");
-    delay(500);
 }
