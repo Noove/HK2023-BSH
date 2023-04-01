c5e8242b9a4fd50cdf7538814dff7c1d094ab7be
Jakub Nagy <nagykubko@gmail.com>
2023-04-01 20:37:25 +0200

    IMU Demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 // Includes
 #include <Visual/LED.h>
 #include <Visual/Matrix.h>
+#include <LSM6DSOSensor.h>
  
+LSM6DSOSensor AccGyr(&Wire);
+
 void setup()
 {
     Serial.begin(115200);
@@ -11,6 +14,10 @@ void setup()
     Wire.begin();
     LED::begin();
 
+    AccGyr.begin();
+    AccGyr.Enable_X();  
+    AccGyr.Enable_G();
+
     for (uint8_t index = 0; index < 4; index++)
     {
         Matrix::set_pixel(2, index, 0, 100, 100);
@@ -19,4 +26,15 @@ void setup()
  
 void loop()
 {
+    int32_t accelerometer[3];
+    int32_t gyroscope[3];
+    AccGyr.Get_X_Axes(accelerometer);  
+    AccGyr.Get_G_Axes(gyroscope);
+
+    Serial.print(accelerometer[0]);
+    Serial.print(",");
+    Serial.print(gyroscope[1]);
+    Serial.print(",");
+    Serial.println(gyroscope[2]);
+    delay(100);
 }
